ebd5dae81530c170b21813873ef248ee9eebe921
Alvaro Rodrigues <alvarorodrigues1710@gmail.com>
2020-01-23 19:45:12 +0000

    Updated test to use proper assert

--- src/test/java/com/programmingTraining/rest/webservices/restfulwebservices/controller/UserResourcesControllerTest.java
+++ src/test/java/com/programmingTraining/rest/webservices/restfulwebservices/controller/UserResourcesControllerTest.java
@@ -3,6 +3,7 @@ package com.programmingTraining.rest.webservices.restfulwebservices.controller;
 import com.programmingTraining.rest.webservices.restfulwebservices.RestfulWebServicesApplication;
 import io.restassured.RestAssured;
 import io.restassured.response.Response;
+import org.apache.http.HttpStatus;
 import org.hamcrest.core.Is;
 import org.hamcrest.core.IsNull;
 import org.junit.Before;
@@ -13,6 +14,8 @@ import org.springframework.boot.web.server.LocalServerPort;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
 import static io.restassured.RestAssured.given;
+import static org.apache.http.HttpStatus.*;
+import static org.hamcrest.core.Is.is;
 import static org.junit.Assert.assertThat;
 
 @RunWith(SpringJUnit4ClassRunner.class)
@@ -36,15 +39,15 @@ public class UserResourcesControllerTest {
         response = given().log().all()
                 .when().get("/users").thenReturn();
 
-        assertThat(response.getBody().asString(), Is.is(IsNull.notNullValue()));
-        assertThat(response.getStatusCode(), Is.is(200));
+        assertThat(response.getBody().asString(), is(IsNull.notNullValue()));
+        assertThat(response.getStatusCode(), is(SC_OK));
     }
 
     @Test
     public void shouldRetrieveUserById() {
         given().log().all()
                 .when().get("/users/10000")
-                .then().log().all().statusCode(200);
+                .then().log().all().statusCode(SC_OK);
     }
 
     @Test
@@ -55,7 +58,7 @@ public class UserResourcesControllerTest {
                 .when().post("/users/").thenReturn();
         response.then().log().all();
 
-        assertThat(response.statusCode(), Is.is(201));
+        assertThat(response.statusCode(), is(SC_CREATED));
     }
 
     @Test
@@ -64,9 +67,9 @@ public class UserResourcesControllerTest {
                 .when().get("/users/10000/posts");
         response.then().log().all();
 
-        assertThat(response.getBody().asString().contains("10000"), Is.is(true));
-        assertThat(response.getBody().asString().contains("20000"), Is.is(true));
-        assertThat(response.statusCode(), Is.is(200));
+        assertThat(response.getBody().asString().contains("10000"), is(true));
+        assertThat(response.getBody().asString().contains("20000"), is(true));
+        assertThat(response.statusCode(), is(SC_OK));
     }
 
     @Test
@@ -75,8 +78,8 @@ public class UserResourcesControllerTest {
                 .when().get("/users/10000/posts/10000").thenReturn();
         response.then().log().all();
 
-        assertThat(response.getBody().asString().contains("Hola mundo"), Is.is(true));
-        assertThat(response.statusCode(), Is.is(200));
+        assertThat(response.getBody().asString().contains("Hola mundo"), is(true));
+        assertThat(response.statusCode(), is(SC_OK));
     }
 
     @Test
@@ -87,6 +90,6 @@ public class UserResourcesControllerTest {
                 .when().get("/users/10000/posts/").thenReturn();
         response.then().log().all();
 
-        assertThat(response.statusCode(), Is.is(201));
+        assertThat(response.statusCode(), is(SC_CREATED));
     }
 }
--- src/test/java/com/programmingTraining/rest/webservices/restfulwebservices/exceptionhandling/CustomResponseEntityExceptionHandlerTest.java
+++ src/test/java/com/programmingTraining/rest/webservices/restfulwebservices/exceptionhandling/CustomResponseEntityExceptionHandlerTest.java
@@ -1,10 +1,13 @@
 package com.programmingTraining.rest.webservices.restfulwebservices.exceptionhandling;
 
 import com.programmingTraining.rest.webservices.restfulwebservices.IntegrationTestBase;
+import org.apache.http.HttpStatus;
 import org.hamcrest.core.Is;
 import org.junit.Test;
 
 import static io.restassured.RestAssured.given;
+import static org.apache.http.HttpStatus.*;
+import static org.hamcrest.core.Is.is;
 import static org.junit.Assert.assertThat;
 
 public class CustomResponseEntityExceptionHandlerTest extends IntegrationTestBase {
@@ -14,18 +17,18 @@ public class CustomResponseEntityExceptionHandlerTest extends IntegrationTestBas
     public void shouldThrowUserNotFoundException() {
         String invalidUserId = "12345";
         response = given().get("/users/" + invalidUserId).andReturn();
-        assertThat(response.getStatusCode(), Is.is(404));
+        assertThat(response.getStatusCode(), is(SC_NOT_FOUND));
         assertThat(response.getBody().asString()
-                .contains("User with id: '" + invalidUserId + "' was not found."), Is.is(true));
+                .contains("User with id: '" + invalidUserId + "' was not found."), is(true));
     }
 
     @Test
     public void shouldThrowPostNotFoundException() {
         String invalidPostId = "12345";
         response = given().get("/users/1/posts/" + invalidPostId).andReturn();
-        assertThat(response.getStatusCode(), Is.is(404));
+        assertThat(response.getStatusCode(), is(SC_NOT_FOUND));
         assertThat(response.getBody().asString()
-                .contains("Post with id: '" + invalidPostId + "' was not found."), Is.is(true));
+                .contains("Post with id: '" + invalidPostId + "' was not found."), is(true));
     }
 
 
